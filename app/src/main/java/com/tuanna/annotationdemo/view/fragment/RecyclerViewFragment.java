82e5bd1e4ba519f1b44f1a5728800291eaf6ac96
Nguyen Anh Tuan <tuanna@asiantech.vn>
2015-10-27 16:15:58 +0700

    create DetailFragment

--- app/src/main/java/com/tuanna/annotationdemo/view/fragment/RecyclerViewFragment.java
+++ app/src/main/java/com/tuanna/annotationdemo/view/fragment/RecyclerViewFragment.java
@@ -14,6 +14,6 @@ import org.androidannotations.annotations.EFragment;
  * AnnotationDemo.
  * Created by tuanna on 27/10/2015.
  */
-@EFragment(R.layout.recycler_view_fragment)
+@EFragment(R.layout.fragment_recycler_view)
 public class RecyclerViewFragment extends Fragment{
 }
